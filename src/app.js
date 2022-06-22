7ed27800ae16747f74984e25f12d53d6df8d9468
warbler <1741847465@qq.com>
2022-06-22 15:37:00 +0800

    test

--- src/app.js
+++ src/app.js
@@ -2,57 +2,61 @@
  * @Author: 一尾流莺
  * @Description:
  * @Date: 2022-06-17 14:34:19
- * @LastEditTime: 2022-06-19 14:41:24
+ * @LastEditTime: 2022-06-22 15:08:49
  * @FilePath: \biz-editer-server\src\app.js
  */
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-const jwt = require('./middlewares/jwt')
+const Koa = require("koa");
 
-const index = require('../src/routes/index')
-const users = require('../src/routes/users')
+const app = new Koa();
+const views = require("koa-views");
+const json = require("koa-json");
+const onerror = require("koa-onerror");
+const bodyparser = require("koa-bodyparser");
+const logger = require("koa-logger");
+const jwt = require("./middlewares/jwt");
+
+const index = require("./routes/index");
+const users = require("./routes/users");
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
+app.use(
+  bodyparser({
+    enableTypes: ["json", "form", "text"],
+  })
+);
 
 // 配置 jwt 中间件
 
-app.use(jwt)
+app.use(jwt);
 
-app.use(json())
-app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(json());
+app.use(logger());
+app.use(require("koa-static")(`${__dirname}/public`));
 
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
+app.use(
+  views(`${__dirname}/views`, {
+    extension: "pug",
+  })
+);
 
 // logger
 app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-
-// routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
-
-// error-handling
-app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
 });
 
+// routes
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
-module.exports = app
+// error-handling
+app.on("error", (err, ctx) => {
+  console.error("server error", err, ctx);
+});
+
+module.exports = app;
