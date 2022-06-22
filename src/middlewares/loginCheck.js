1fbc5f3239b5201dd7d23f67180a402cb678aa05
warbler <1741847465@qq.com>
2022-06-22 15:37:40 +0800

    test

--- src/middlewares/loginCheck.js
+++ src/middlewares/loginCheck.js
@@ -1,9 +1,9 @@
 // 解密
-const { jwtVerify } = require('../utils/jwt')
+const { jwtVerify } = require("../utils/jwt");
 // 执行失败的数据模型
-const { ErrorRes } = require('../res-model/index')
+const { ErrorRes } = require("../res-model/index");
 //
-const { loginCheckFailInfo } = require('../res-model/failInfo/index')
+const { loginCheckFailInfo } = require("../res-model/failInfo/index");
 
 /**
  * 登录校验
@@ -12,29 +12,29 @@ const { loginCheckFailInfo } = require('../res-model/failInfo/index')
  */
 module.exports = async function loginCheck(ctx, next) {
   // 失败信息
-  const errRes = new ErrorRes(loginCheckFailInfo)
+  const errRes = new ErrorRes(loginCheckFailInfo);
 
   // 获取 token
-  const token = ctx.header.authorization
+  const token = ctx.header.authorization;
   if (!token) {
-    ctx.body = errRes
-    return
+    ctx.body = errRes;
+    return;
   }
 
-  let flag = true
+  let flag = true;
   try {
-    const userInfo = await jwtVerify(token)
-    delete userInfo.password // 屏蔽密码
+    const userInfo = await jwtVerify(token);
+    delete userInfo.password; // 屏蔽密码
 
     // 验证成功，获取 userInfo
-    ctx.userInfo = userInfo
+    ctx.userInfo = userInfo;
   } catch (ex) {
-    flag = false
-    ctx.body = errRes
+    flag = false;
+    ctx.body = errRes;
   }
 
   if (flag) {
     // 继续下一步
-    await next()
+    await next();
   }
-}
+};
